89ff64badb30a31158fc3798cadea1b43cad0669
felipe vera <felipevera10@gmail.com>
2017-08-30 14:53:34 -0300

    seed añadido

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.destroy_all
+
+cat1 = Category.create!(name: "Categoría 1")
+cat2 = Category.create!(name: "Categoría 2")
+cat3 = Category.create!(name: "Categoría 3")
+
+cat1.products.build(name: "Producto 1", price: 1990)
+cat2.products.build(name: "Producto 2", price: 2990)
+cat3.products.build(name: "Producto 3", price: 5990)
+
+Product.create!(name: "Producto 4", price: 3599, category_id: cat1.id)
+Product.create!(name: "Producto 5", price: 11990, category_id: cat2.id)
+prod = Product.create!(name: "Producto 6", price: 35000, category_id: cat3.id)
+
+Product.last.destroy 
+
+prod = Product.last
+prod.premium = true
+prod.save
+
+Category.all.each do |category|
+	category.name = category.name.upcase
+	category.save
+end
